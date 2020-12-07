1ae5635588050ca33028630013f5d82335de260a
Jason Hinchcliffe <hinchy@gmail.com>
2020-12-07 22:11:15 +1100

    finished part 1 of day 6

---
+++ day_6/day_6.rb
@@ -0,0 +1,12 @@
+require_relative "../file_loader.rb"
+
+all_lines = FileLoader.new("./day_6/input.txt").lines(->(x) { x.delete("\n") })
+
+def clean_data(file_lines)
+  file_lines.inject([""]) do |acc, line|
+    line.empty? ? acc << "" : acc.last << line
+    acc
+  end
+end
+
+pp clean_data(all_lines).sum{|l| l.split("").uniq.length}
